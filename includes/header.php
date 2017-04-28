bc7dec965a60f0cdf9529c98d9bcda9605c50bdf
engine77 <zaur.1kager@gmail.com>
2017-04-28 18:47:32 +0400

    order

--- includes/header.php
+++ includes/header.php
@@ -18,6 +18,7 @@
 	<link rel="stylesheet" href="block/button/button.css">
 	<link rel="stylesheet" href="block/inputtext/style.css">
 	<link rel="stylesheet" href="block/order/style.css">
+	<link rel="stylesheet" href="block/socials/socials.css">
 
 
 	<link rel="stylesheet" href="css/style-db.css">
