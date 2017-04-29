be3c29e5890805d7fe3b1807723298cc4eddc336
Berzeg07 <berezg86@gmail.com>
2017-04-29 10:05:36 +0400

    29.14

--- includes/extra.php
+++ includes/extra.php
@@ -4,35 +4,35 @@
 		<div class="row">
 
 			<div class="col-md-12">
-				<h1>Дополнительные услуги</h1>
+			<h2 class="h2 h2_white h2_white_pb">Дополнительные услуги</h2>
 			</div>
 			<div class="clearfix"></div>		
 
-			<div class="col-md-4">
-				<div class="services services__view">
+			<div class="col-md-4 col-sm-4">
+				<div class="services services__view services__view_first">
 					<div class="services__item">
 						<img src="img/p1.png" alt="">
 					</div>
 					<div class="services__item">
-						<p>Консультирование клиентов по выбору конвейера с выездом специалиста на территорию заказчика
+						<p>Консультирование клиентов по<br class="br"/> выбору конвейера с выездом<br class="br"/> специалиста на территорию<br class="br"/> заказчика
 						</p>
 					</div>
 				</div>
 			</div>
 
-			<div class="col-md-4">
-				<div class="services services__view">
+			<div class="col-md-4 col-sm-4">
+				<div class="services services__view services__view_first">
 					<div class="services__item">
 						<img src="img/p2.png" alt="">
 					</div>
 					<div class="services__item">
-						<p>Составление технического задания и произведение необходимых замеров</p>
+						<p>Составление технического задания <br/>и произведение необходимых замеров</p>
 					</div>
 				</div>
 			</div>
 
-			<div class="col-md-4">
-				<div class="services services__view">
+			<div class="col-md-4 col-sm-4">
+				<div class="services services__view services__view_first">
 					<div class="services__item">
 						<img src="img/p3.png" alt="">
 					</div>
@@ -45,7 +45,7 @@
 			<div class="clearfix"></div>
 
 
-			<div class="col-md-4">
+			<div class="col-md-4 col-sm-4">
 				<div class="services services__view">
 					<div class="services__item">
 						<img src="img/p4.png" alt="">
@@ -56,7 +56,7 @@
 				</div>
 			</div>
 
-			<div class="col-md-4">
+			<div class="col-md-4 col-sm-4">
 				<div class="services services__view">
 					<div class="services__item">
 						<img src="img/p5.png" alt="">
@@ -67,7 +67,7 @@
 				</div>
 			</div>
 
-			<div class="col-md-4">
+			<div class="col-md-4 col-sm-4">
 				<div class="services services__view">
 					<div class="services__item">
 						<img src="img/p6.png" alt="">
