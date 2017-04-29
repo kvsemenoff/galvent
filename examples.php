1a915efc0c4a468ae28a864945ca966fdb5474c5
engine77 <zaur.1kager@gmail.com>
2017-04-29 18:08:22 +0400

    forms

--- examples.php
+++ examples.php
@@ -12,7 +12,7 @@
 	<span class="order_curcle__text">заявка</span>
 </a>
 
-<div class="container">	
+<div class="container center container_margin">	
 	<h2 class="h2 h2_dark examples__caption">Примеры наших рaбот</h2>
 
 	<div class="examples__block">
@@ -20,35 +20,51 @@
 		<div class="examples__slider">
 			<div class="item">
 				<div class="examples__img">
-					<img src="img/examples1.jpg" alt="">
+					<a href="img/examples1.jpg" rel="group" class="fancybox"><img src="img/examples1.jpg" alt=""></a>
 				</div>
 			</div>
 			<div class="item">
 				<div class="examples__img">
-					<img src="img/examples1.jpg" alt="">
+					<a href="img/examples1.jpg" rel="group"><img src="img/examples1.jpg" alt=""></a>
+				</div>
+			</div>
+		</div>
+		<p class="examples__block_text">Основная сфера деятельности нашего предприятия – выпуск конвейеров и рольгангов. Наличие высококвалифицированных специалистов  позволяет спроектировать и изготовить оборудование, которое будет максимально соответствовать Вашим требованиям. Мы специализируемся на изготовлении и продаже конвейерных систем различных видов, подходящих практически для всех отраслей промышленности.</p>
+	</div>	
+	<div class="examples__block">
+		<h4 class="examples__block_caption">Установка конвейеров для ООО ХлебоПром</h4>
+		<div class="examples__slider">
+			<div class="item">
+				<div class="examples__img">
+					<a href="img/examples2.jpg" rel="group"><img src="img/examples2.jpg" alt=""></a>
+				</div>
+			</div>
+			<div class="item">
+				<div class="examples__img">
+					<a href="img/examples1.jpg" rel="group"><img src="img/examples1.jpg" alt=""></a>
+				</div>
+			</div>
+		</div>
+		<p class="examples__block_text">Основная сфера деятельности нашего предприятия – выпуск конвейеров и рольгангов. Наличие высококвалифицированных специалистов  позволяет спроектировать и изготовить оборудование, которое будет максимально соответствовать Вашим требованиям. Мы специализируемся на изготовлении и продаже конвейерных систем различных видов, подходящих практически для всех отраслей промышленности.</p>
+	</div>
+	<div class="examples__block">
+		<h4 class="examples__block_caption">Установка конвейеров для ООО ХлебоПром</h4>
+		<div class="examples__slider">
+			<div class="item">
+				<div class="examples__img">
+					<a href="img/examples3.jpg" rel="group"><img src="img/examples3.jpg" alt=""></a>
+				</div>
+			</div>
+			<div class="item">
+				<div class="examples__img">
+					<a href="img/examples1.jpg" rel="group"><img src="img/examples1.jpg" alt=""></a>
 				</div>
 			</div>
 		</div>
 		<p class="examples__block_text">Основная сфера деятельности нашего предприятия – выпуск конвейеров и рольгангов. Наличие высококвалифицированных специалистов  позволяет спроектировать и изготовить оборудование, которое будет максимально соответствовать Вашим требованиям. Мы специализируемся на изготовлении и продаже конвейерных систем различных видов, подходящих практически для всех отраслей промышленности.</p>
 	</div>
 	
-	<div class="examples__block">
-		<h4 class="examples__block_caption">Установка конвейеров для ООО ХлебоПром</h4>
-		<div class="examples__slider">
-			<div class="item">
-				<div class="examples__img">
-					<img src="img/examples1.jpg" alt="">
-				</div>
-			</div>
-			<div class="item">
-				<div class="examples__img">
-					<img src="img/examples1.jpg" alt="">
-				</div>
-			</div>
-		</div>
-		<p class="examples__block_text">Основная сфера деятельности нашего предприятия – выпуск конвейеров и рольгангов. Наличие высококвалифицированных специалистов  позволяет спроектировать и изготовить оборудование, которое будет максимально соответствовать Вашим требованиям. Мы специализируемся на изготовлении и продаже конвейерных систем различных видов, подходящих практически для всех отраслей промышленности.</p>
-	</div>
-
+	<span class="examples__look_more">смотреть еще работы</span>
 </div>
 
 <?php require_once('includes/footer.php'); ?>
--- js/common.js
+++ js/common.js
@@ -135,6 +135,14 @@ $(document).ready(function(){
 		closeEffect	: 'true'
 	});
 
+	$("a[rel=group]").fancybox({
+		'transitionIn' : 'none',
+		'transitionOut' : 'none',
+		'titlePosition' : 'over',
+		'titleFormat' : function(title, currentArray, currentIndex, currentOpts) {
+			return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+		}
+	});
 });
 
 
