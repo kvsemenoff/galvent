a53ef34f6e55265160f4c25442ca75334abbfb98
Berzeg07 <berezg86@gmail.com>
2017-04-30 13:21:32 +0400

    29.14

--- garant.php
+++ garant.php
@@ -11,10 +11,12 @@
 
 <div class="service-wrap">
 	<div class="container relative">
+
 		<a href="#" class="order_curcle_innerpage">
 			<span class="order_curcle__text toppadding">On-line</span>
 			<span class="order_curcle__text">заявка</span>
 		</a>
+
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12">
 				<h2 class="h2 h2_dark">Гарантия</h2>
@@ -22,20 +24,49 @@
 			<div class="clearfix"></div>
 
 			<div class="col-xs-12 col-sm-12 col-md-12">
-				<div class="service-box service-box__view">
-					<span>«ПТО Галвент» обеспечивает гарантии высокого качества:</span>
+				<div class="service-box service-box__view service-box__view_garant">
+					<span class="service-box__view_span service-box__view_span_2">«ПТО Галвент» обеспечивает гарантии высокого качества:</span>
+
 					<div class="service-box__inner">
-						<ul>
+
+						<ul class="service-box__view_ul">
 							<li><span>материалов и оборудования, использованных в производстве конвейеров;</span></li>
 							<li><span>произведённых конвейерных систем;</span></li>
 							<li><span>монтажных и сборочных работ.</span></li>
 							<li><span>Благодаря мощному производственному потенциалу, высокой квалификации сотрудников, постоянному контролю качества на всех этапах производственного процесса мы можем гарантировать бесперебойную работу оборудования в течение 24 месяцев!
 							</span></li>
 						</ul>
+
 					</div>
+
+					<div class="strv"><img src="img/strv.png" alt=""></div>
+				</div>
+
+				<div class="service-box service-box__view service-box__view_garant_2">
+					<span class="service-box__view_span service-box__view_span_3">Правила выполнения гарантийных обязательств</span>
+
+					<div class="service-box__inner">
+
+						<ol class="service-box__view_ol ol ol_theme_rosa ol_color">
+							<li class="ol_theme_rosa__li "><span>Гарантии распространяются на всю продукцию компании «ПТО Галвент» и действительны в течение 2-х лет с момента отгрузки оборудования при условии соблюдения правил эксплуатации.</span></li>
+							<li class="ol_theme_rosa__li "><span>«ПТО Галвент» гарантирует покупателю оборудования, на которое распространяется гарантия, бесплатную замену или ремонт любого дефектного узла оборудования.</span></li>
+							<li class="ol_theme_rosa__li "><span>В гарантию включаются стоимость запасных частей и расходы на работу. Доставка дефектных частей и проезд специалистов к заказчику за пределы г. Москвы оплачивается покупателем отдельно. </span></li>
+							<li class="ol_theme_rosa__li "><span>Компания «ПТО Галвент» оставляет за собой право экспертизы для определения причины поломки. Если экспертиза покажет, что поломка входит в гарантийный случай, компания обязуется провести ремонт или замену оборудования.
+							</span></li>
+							<li class="ol_theme_rosa__li "><span>Гарантия не распространяется на случаи выхода из строя оборудования или отдельных его частей из-за неправильного самостоятельного монтажа оборудования, порчи от коррозии или абразивных материалов, неправильного подключения оборудования к электросети и иных случаев, не предусмотренных правилами эксплуатации.
+							</span></li>
+							<li class="ol_theme_rosa__li "><span>В случае введения покупателем любых изменений или модификаций в конструкцию оборудования, гарантия автоматически теряет силу. 
+							</span></li>
+							<li class="ol_theme_rosa__li "><span>Любые рекламации вступают в силу при условии, что они надлежащим образом оформлены и предоставлены компании «ПТО Галвент» в письменной форме.
+							</span></li>
+						</ol>
+
+					</div>
+
 				</div>
 			</div>
 			<div class="clearfix"></div>
+
 
 			<div class="col-xs-12 col-sm-12 col-md-12">
 				<div class="form__title">
