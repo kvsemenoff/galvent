360e8634aecb07bbac5d2503e5ceb4030a86ac37
engine77 <zaur.1kager@gmail.com>
2017-05-01 15:48:00 +0400

    all file

--- includes/fast.php
+++ includes/fast.php
@@ -17,21 +17,21 @@
 					<form action="#" method="post" class="js-submit">		
 						<div class="form_for_main_page__left">
 							<label for="main-uname" class="for_input">Ваше ФИО:<i>*</i></label>
-							<span class="input input_theme_main"><input type="text" class="input__control" id="main-uname" name="uname" placeholder="Василий" required></span> 
+							<span class="input input_theme_main"><input type="text" class="input__control" name="uname" placeholder="Василий" required></span> 
 
 							<label for="main-phone" class="for_input">Телефон:<i>*</i></label>
-							<span class="input input_theme_main"><input type="tel" id="main-phone" class="input__control js-phone" name="phone" placeholder="+7(___)___-__-__" required></span>
+							<span class="input input_theme_main"><input type="tel" class="input__control js-phone" name="phone" placeholder="+7(___)___-__-__" required></span>
 						</div>
 						<div class="form_for_main_page__right">
 							<label for="main-email" class="for_input">E-mail:<i>*</i></label>
-							<span class="input input_theme_main"><input type="email" id="main-email" class="input__control" name="email" placeholder="vasiliy@mail.ru" required></span> 
+							<span class="input input_theme_main"><input type="email" class="input__control" name="email" placeholder="vasiliy@mail.ru" required></span> 
 
 							<label for="thememessage" class="for_input">Тема сообщения:<i>*</i></label>
-							<span class="input input_theme_main"><input type="text" id="thememessage" class="input__control" name="thememessage" placeholder="Заказ"></span>
+							<span class="input input_theme_main"><input type="text" class="input__control" name="thememessage" placeholder="Заказ"></span>
 						</div>
 						<div class="clear"></div>
 						<label for="textmessage" class="for_input">Комментарий:<i>*</i></label>
-						<textarea name="message" id="textmessage" class="message message_theme_rosa" placeholder="Текст комментария"></textarea>
+						<textarea name="message" class="message message_theme_rosa" placeholder="Текст комментария"></textarea>
 						<div class="fast__button">
 							<button class="button button__view"><span class="button__view_ico button__view_ico2"></span><span class="button__view_txt button__view_txt3">отправить</span></button>
 						</div>
