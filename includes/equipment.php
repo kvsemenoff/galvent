c40a0e043b7db8dbad8cc40eae0342f52b3714f8
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-02 16:58:49 +0600

    fix

--- includes/equipment.php
+++ includes/equipment.php
@@ -5,5 +5,11 @@
 		<div class="equipment__button">
 			<a href="#" class="button button__view"><span class="button__view_ico"></span><span class="button__view_txt">перейти в каталог</span></a>
 		</div>
+		<div class="equipment_order">
+			<a href="#modal__form_order" class="fancybox order_curcle">
+				<span class="order_curcle__text toppadding">On-line</span>
+				<span class="order_curcle__text">заявка</span>
+			</a>
+		</div>
 	</div>
 </div>
