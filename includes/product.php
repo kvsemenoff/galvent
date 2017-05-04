4b9eaa23e5a704e75236cad3ba57af23a5574345
Almaz Dzhanaliev <aldjanaliev@gmail.com>
2017-05-04 11:42:12 +0600

    fix product

--- includes/product.php
+++ includes/product.php
@@ -6,8 +6,9 @@
 				<div class="product__item product__item_first">
 					<a href="#" class="item__inner">
 						<span class="item__title item__title_first">Ленточные конвейеры</span>
-						<div class="item__img item__img_first">
-							<img src="img/item__img-1.png" alt="">
+						<div class="item__image item__image_first">
+							<img class="item__img item__img-des" src="img/item__img-1.png" alt="">
+							<img class="item__img item__img-mob" src="img/item__img-1-mob.png" alt="">
 						</div>
 						<img class="item__bg" src="img/product__item-1.jpg" alt="">
 					</a>
@@ -15,8 +16,9 @@
 				<div class="product__item product__item_second">
 					<a href="#" class="item__inner">
 						<span class="item__title item__title_second">Комплектующие</span>
-						<div class="item__img item__img_second">
-							<img src="img/item__img-2.png" alt="">
+						<div class="item__image item__image_second">
+							<img class="item__img item__img-des" src="img/item__img-2.png" alt="">
+							<img class="item__img item__img-mob" src="img/item__img-2-mob.png" alt="">
 						</div>
 						<img class="item__bg" src="img/product__item-2.jpg" alt="">
 					</a>
@@ -26,8 +28,9 @@
 				<div class="product__item product__item_third product__item_big">
 					<a href="#" class="item__inner">
 						<span class="item__title item__title_third">Роликовые конвейеры — рольганги</span>
-						<div class="item__img item__img_third">
-							<img src="img/item__img-3.png" alt="">
+						<div class="item__image item__image_third">
+							<img class="item__img item__img-des" src="img/item__img-3.png" alt="">
+							<img class="item__img item__img-mob" src="img/item__img-3-mob.png" alt="">
 						</div>
 						<img class="item__bg" src="img/product__item-3.jpg" alt="">
 					</a>
